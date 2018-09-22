4f402bf938d700a1439e93d6204961fbe515dfb5
mib348 <ibrahimbutt348@gmail.com>
2018-09-22 10:46:07 +0500

    created taxonomies

--- wp-content/themes/unite-child/functions.php
+++ wp-content/themes/unite-child/functions.php
@@ -14,21 +14,72 @@ add_action( 'wp_enqueue_scripts', 'chld_thm_cfg_parent_css', 10 );
 
 // END ENQUEUE PARENT ACTION
 
-// Register Custom Post Type
-function create_posttype() {
+function awesome_custom_post_type (){
     
-    register_post_type( 'films',
-        // CPT Options
-        array(
-            'labels' => array(
-                'name' => __( 'Films' ),
-                'singular_name' => __( 'Film' )
-            ),
-            'public' => true,
-            'has_archive' => true,
-            'rewrite' => array('slug' => 'films'),
-        )
-        );
+    $labels = array(
+        'name' => 'Films',
+        'singular_name' => 'Film',
+        'add_new' => 'Add Film',
+        'all_Films' => 'All Films',
+        'add_new_item' => 'Add Film',
+        'edit_item' => 'Edit Film',
+        'new_item' => 'New Film',
+        'view_item' => 'View Film',
+        'search_item' => 'Search Films',
+        'not_found' => 'No Film found',
+        'not_found_in_trash' => 'No Films found in trash',
+        'parent_item_colon' => 'Parent Film'
+    );
+    $args = array(
+        'labels' => $labels,
+        'public' => true,
+        'has_archive' => true,
+        'publicly_queryable' => true,
+        'query_var' => true,
+        'rewrite' => true,
+        'capability_type' => 'post',
+        'hierarchical' => false,
+        'supports' => array(
+            'title',
+            'editor',
+            'excerpt',
+            'thumbnail',
+            'revisions',
+        ),
+        'menu_position' => 5,
+        'exclude_from_search' => false
+    );
+    register_post_type('films',$args);
 }
-// Hooking up our function to theme setup
-add_action( 'init', 'create_posttype' );
+add_action('init','awesome_custom_post_type');
+
+
+
+function awesome_custom_taxonomies() {
+    
+    register_taxonomy('genre', 'films', array(
+        'label' => 'Genre',
+        'rewrite' => array( 'slug' => 'genre' ),
+        'hierarchical' => false
+    ) );
+    
+    register_taxonomy('year', 'films', array(
+        'label' => 'Year',
+        'rewrite' => array( 'slug' => 'year' ),
+        'hierarchical' => false
+    ) );
+    
+    register_taxonomy('actor', 'films', array(
+        'label' => 'Actor',
+        'rewrite' => array( 'slug' => 'actor' ),
+        'hierarchical' => false
+    ) );
+    
+    register_taxonomy('country', 'films', array(
+        'label' => 'Country',
+        'rewrite' => array( 'slug' => 'country' ),
+        'hierarchical' => false
+    ) );
+    
+}
+add_action( 'init' , 'awesome_custom_taxonomies' );
